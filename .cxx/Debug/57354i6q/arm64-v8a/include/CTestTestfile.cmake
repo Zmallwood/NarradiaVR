b75933e4f817bc2fc65a57f4e7fbc02638914ae9
Zmallwood <zmallwood@proton.me>
2023-10-26 19:45:59 +0200

    Save

--- .cxx/Debug/57354i6q/arm64-v8a/include/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andreas/Code/C++/VR/OpenXR/include
-# Build directory: /home/andreas/Code/C++/VR/OpenXR/src/tests/Forradia/.cxx/Debug/57354i6q/arm64-v8a/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("openxr")
--- .cxx/Debug/57354i6q/armeabi-v7a/src/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andreas/Code/C++/VR/OpenXR/src/tests
-# Build directory: /home/andreas/Code/C++/VR/OpenXR/src/tests/Forradia/.cxx/Debug/57354i6q/armeabi-v7a/src/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Forradia")
